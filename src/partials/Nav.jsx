bceac90958da9b5947dc78edbc61d8dc79abfdd8
Muhammad Haris Khan <mhkhan.bscs19seecs@seecs.edu.pk>
2022-07-12 12:22:42 +0500

    changed padding and bg color of navbar:

--- src/partials/Nav.jsx
+++ src/partials/Nav.jsx
@@ -17,7 +17,10 @@ const Nav = () => {
 
   return (
     <div className="fixed w-full z-30 bg-white backdrop-blur-sm shadow-lg md:shadow-sm top-0 left-0 w-full">
-      <div className=" md:flex bg-[#f3f3f3] text-black   items-center justify-between py-8 md:px-5 px-7">
+      <div
+        className=" md:flex bg-white text-black   items-center justify-between  md:px-5 px-7"
+        style={{ paddingTop: "30px", paddingBottom: "30px" }}
+      >
         <div className="font-bold ml-4 text-2xl cursor-pointer flex items-center font-sans">
           <p className="text-3xl mr-2"></p>
           <img src={imageNavbar} alt="bug" height={50} width={150} />
@@ -29,7 +32,7 @@ const Nav = () => {
           <FontAwesomeIcon icon={Open ? faXmark : faBars} />
         </div>
         <ul
-          className={`md:flex md:items-center md:pb-0 pb-5 absolute md:static bg-[#f3f3f3] md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-7 transition-all duration-500 ease-in  ${
+          className={`md:flex md:items-center md:pb-0 pb-5 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-7 transition-all duration-500 ease-in  ${
             Open
               ? "top-16 opacity-100 bg-white"
               : "top-[-490px] md:opacity-100 opacity-0"
